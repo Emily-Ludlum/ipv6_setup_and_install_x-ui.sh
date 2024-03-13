c579ec5883984b08e6f249cafee76bfaf4c57960
Emily-Ludlum <160514488+Emily-Ludlum@users.noreply.github.com>
2024-03-13 10:22:58 -0600

    ipv6_setup_and_install_x-ui.sh

--- ipv6_setup_and_install_x-ui.sh
+++ ipv6_setup_and_install_x-ui.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# 默认设置
+DEFAULT_DOMAIN="example.com"
+DEFAULT_EMAIL="your_email@example.com"
+
+# 询问用户输入域名和邮箱地址
+read -p "请输入要申请 SSL 证书的域名（默认值：$DEFAULT_DOMAIN）: " DOMAIN
+read -p "请输入 SSL 证书的邮箱地址（默认值：$DEFAULT_EMAIL）: " EMAIL
+
+# 如果用户未输入任何内容，则使用默认设置
+DOMAIN=${DOMAIN:-$DEFAULT_DOMAIN}
+EMAIL=${EMAIL:-$DEFAULT_EMAIL}
+
 # 设置防火墙规则
 echo "设置防火墙规则..."
 iptables -P INPUT ACCEPT
@@ -23,18 +35,6 @@ wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh
 # 安装签发证书工具
 echo "安装签发证书工具..."
 curl https://get.acme.sh | sh
-
-# 默认设置
-DEFAULT_DOMAIN="example.com"
-DEFAULT_EMAIL="your_email@example.com"
-
-# 询问用户输入域名和邮箱地址
-read -p "请输入要申请 SSL 证书的域名（默认值：$DEFAULT_DOMAIN）: " DOMAIN
-read -p "请输入 SSL 证书的邮箱地址（默认值：$DEFAULT_EMAIL）: " EMAIL
-
-# 如果用户未输入任何内容，则使用默认设置
-DOMAIN=${DOMAIN:-$DEFAULT_DOMAIN}
-EMAIL=${EMAIL:-$DEFAULT_EMAIL}
 
 # 申请 SSL 证书，并下载到服务器目录
 echo "申请 SSL 证书，并下载到服务器目录..."
